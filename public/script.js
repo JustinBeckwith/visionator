cdbfbd1a4984dc79072a23e0622d23dee61f9926
Justin Beckwith <beckwith@google.com>
2016-07-26 09:39:36 -0500

    add face recognition

--- public/script.js
+++ public/script.js
@@ -1,39 +1,18 @@
 var width = 320;
 var height = 0;
-var streaming = false;
-var video = null;
-var canvas = null;
-var photo = null;
-var startbutton = null;
-var okBtn = null;
-var message = null;
-var clearImage = false;
-var dotdotInterval = null;
+var video, canvas, photo, startbutton, okBtn, streaming,
+    message, clearImage, dotdotInterval, ctx, handle;
 
 function onload() {
   okBtn = document.getElementById('okBtn');
   message = document.getElementById('message');
-  startup();
-  newBring()
-
-  okBtn.addEventListener('click',() => {
-    message.style.display = 'none';
-    if (clearImage) {
-      photo.style.display = 'none';
-      clearImage = false;
-    }
-  });
-}
-
-function newBring() {
-  photo.style.display = 'none';
-}
-
-function startup() {
   video = document.getElementById('video');
-  canvas = document.getElementById('canvas');
   photo = document.getElementById('photo');
   startbutton = document.getElementById('startbutton');
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  ctx.strokeStyle = "#3E82F7";
+  ctx.fillStyle = "#3E82F7";
 
   navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
@@ -44,27 +23,20 @@ function startup() {
       video: true,
       audio: false
     },
-    function(stream) {
+    (stream) => {
       var vendorURL = window.URL || window.webkitURL;
       video.src = vendorURL.createObjectURL(stream);
-      video.play();
+      videoStart();
     },
-    function(err) {
-      console.log("An error occured! " + err);
+    (err) => {
+      console.error("An error occured! " + err);
     }
   );
 
-  video.addEventListener('canplay', function(ev){
+  video.addEventListener('canplay', (ev) => {
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
-    
-      // Firefox currently has a bug where the height can't be read from
-      // the video, so we will make assumptions if this happens.
-    
-      if (isNaN(height)) {
-        height = width / (4/3);
-      }
-    
+      height = video.videoHeight;
+      width = video.videoWidth;
       video.setAttribute('width', width);
       video.setAttribute('height', height);
       canvas.setAttribute('width', width);
@@ -73,23 +45,26 @@ function startup() {
     }
   }, false);
 
-  startbutton.addEventListener('click', function(ev){
+  startbutton.addEventListener('click', (ev) => {
     takepicture();
     ev.preventDefault();
   }, false);
 
+  okBtn.addEventListener('click',() => {
+    message.style.display = 'none';
+    if (clearImage) {
+      clearImage = false;
+      videoStart();
+    }
+  });
 }
 
 function takepicture() {
-  var context = canvas.getContext('2d');
-  canvas.width = width;
-  canvas.height = height;
-  context.drawImage(video, 0, 0, width, height);
-
   var data = canvas.toDataURL('image/png');
   photo.setAttribute('src', data);
   photo.style.height = video.offsetHeight;
-  photo.style.display = "inline-block";
+
+  videoPause();
 
   showMessage('Detecting stuff. We can tell by the pixels o.0', true);
   
@@ -104,9 +79,26 @@ function takepicture() {
   }).then((features) => {
     console.log(features);
     var found = false;
-    var text = features.labels.join(', ');
-    showMessage(text);
+    
+    // show labels if available
+    if (features.labels.length > 0) {
+      var text = features.labels.join(', ');
+      showMessage(text);
+    }
+
     clearImage = true;
+    
+    for (var face of features.faces) {
+      ctx.beginPath();
+      var startPoint = face.bounds.face[face.bounds.face.length-1];
+      ctx.moveTo(startPoint.x, startPoint.y);
+      for (var point of face.bounds.face) {
+        ctx.fillRect(point.x-3, point.y-3, 7, 7);
+        ctx.lineTo(point.x, point.y);
+      };
+      ctx.stroke();
+    }
+
   }).catch((err) => {
     console.error('There was a problem :(');
     console.error(err);
@@ -134,4 +126,18 @@ function hideMessage() {
   if (dotdotInterval) {
     clearInterval(dotdotInterval);
   }
+}
+
+function videoStart() {
+  video.play();
+  handle = setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 20);
+}
+
+function videoPause() {
+  if (handle) {
+    clearInterval(handle);
+  }
+  video.pause();
 }
